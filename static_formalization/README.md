f8e6295e320b5a0dc42007a013c3c8b67f04124a
angelo-casciani <angelocasciani@live.it>
2025-09-18 12:46:56 +0200

    Uploaded formalization code

---
+++ static_formalization/README.md
@@ -0,0 +1,152 @@
+# SitCalc4BPMN
+
+This is the formalization of the case study proposed in the paper "Enabling Formal Reasoning in Business Processes: A Situation Calculus and ConGolog Semantics for BPMN". It features a BPMN business process for a job application, formalized using Situation Calculus and ConGolog, and implemented utilizing IndiGolog.
+
+The process executes in a simulated environment via the simulator device manager `dev/env_sim.pl`. The simulator includes a simple TCLK/TK where the user can issue exogenous actions:
+
+```prolog
+- `job_needed(<id>)`: start event for the application process by applicant <id>.
+- `<task_name>_end(<id>,...)`: exogenous action to conclude a durative task, which can take data objects as additional parameters.
+- `withdrawal_by_applicant(<id>)`: withdrawal of the application by applicant <id>.
+- `shut_down`: used to stop the pools' servers.
+```
+
+The `end_indi` exogenous actions will finish the execution smoothly.
+
+## Setup
+
+First, install the SWI-Prolog system. In Ubuntu-based system, use PPA: https://www.swi-prolog.org/build/PPA.html
+
+We can develop using VSCODE using extension [New-VSC-Prolog](https://marketplace.visualstudio.com/items?itemName=AmauryRabouan.new-vsc-prolog). Seems to be the best extension today for VSCODE; check [post discussion here](https://swi-prolog.discourse.group/t/new-prolog-mode-for-vs-code/7286/12). Once installed, changed the `prolog.executablePath` to `/bin//swipl` in Settings (`Control + ,`).
+
+Clone the [INDIGOLOG repo](https://github.com/ai-krml-uoft/indigolog):
+
+```shell
+$ git clone git@github.com:ai-krml-uoft/indigolog.git
+```
+
+Move the files in the src folder within this repository in the examples folder of Indigolog.
+
+Then, from the main Indigolog folder, consult the application as follows:
+
+```shell
+$ swipl config.pl examples/job_bpmn/main.pl
+```
+
+This will load the domain specification, but also the whole INDIGOLOG architecture (interpreter, temporal projector, devices, libraries, etc.)
+
+Clone the [INDIGOLOG repo](https://github.com/ai-krml-uoft/indigolog):
+
+```shell
+$ git clone git@github.com:ai-krml-uoft/indigolog.git
+```
+
+Test the most simple elevator example:
+
+```shell
+$ swipl indigolog/config.pl indigolog/examples/elevator_simple/main_01.pl
+Welcome to SWI-Prolog (threaded, 64 bits, version 9.2.8)
+SWI-Prolog comes with ABSOLUTELY NO WARRANTY. This is free software.
+Please run ?- license. for legal details.
+
+For online help and background, visit https://www.swi-prolog.org
+For built-in help, use ?- help(Topic). or ?- apropos(Word).
+
+?- main.
+Controllers available: [basic,dumb,smart]
+Select controller: basic.
+
+Executing controller: *basic*
+down
+open
+close
+off(2)
+up
+up
+up
+open
+close
+off(5)
+up
+up
+up
+up
+open
+close
+off(9)
+down
+down
+down
+down
+down
+down
+down
+down
+open
+
+26 actions.
+true .
+```
+
+Here both SWI-Prolog and INDIGOLOG are working! 👍
+
+## Job Application BPMN
+
+We are assuming the INDIGOLOG system has been cloned into `indigolog/` folder.
+
+To run online the Job Application BPMN:
+
+```shell
+$ swipl indigolog/config.pl examples/job_bpmn/main.pl
+SYSTEM(0): Debug level set to 5
+SYSTEM(0): Debug level for module em set to 1
+INFO(0): Set wait-at-action enable to: 1 seconds.
+Welcome to SWI-Prolog (threaded, 64 bits, version 9.2.8)
+SWI-Prolog comes with ABSOLUTELY NO WARRANTY. This is free software.
+Please run ?- license. for legal details.
+
+For online help and background, visit https://www.swi-prolog.org
+For built-in help, use ?- help(Topic). or ?- apropos(Word).
+
+?- main.
+Controllers available: [congolog]
+1. congolog
+
+
+Select controller: 1.
+```
+
+### Reasoning Tasks
+
+These are the queries for the reasoning tasks reported in the AAAI26 submission:
+
+```prolog
+$ swipl indigolog/config.pl examples/job_bpmn/main.pl
+
+?- initialize(evaluator).
+true.
+
+% CONFORMANCE CHECKING
+?- time((H1 = [execute_interview(end, 1, true), execute_interview(start, 1), plan_interview(end, 1), plan_interview(start, 1), check_application_for_interview(end, 1, true), check_application_for_interview(start, 1), assign_contact_partner(end, 1), verify_prerequisites(end, 1), verify_prerequisites(start, 1), assign_contact_partner(start, 1), organize_documents(end, 1), organize_documents(start, 1), check_validity(end, 1, true), check_validity(start, 1), acquire(1, company), application_sent(1), prepare_application(end, 1), prepare_application(start, 1), acquire(1, applicant), job_needed(1)], proc(sim(bpmn_process), E), trans_star(E, [], _, H1))).
+% 203,521 inferences, 0.023 CPU in 0.023 seconds (100% CPU, 8910073 Lips)
+H1 = [execute_interview(end, 1, true), execute_interview(start, 1), plan_interview(end, 1), plan_interview(start, 1), check_application_for_interview(end, 1, true), check_application_for_interview(start, 1), assign_contact_partner(end, 1), verify_prerequisites(end, 1), verify_prerequisites(..., ...)|...],
+E = conc([bpmn_process, end_bpm], exog_actions) .
+
+?- time((H2 = [execute_interview(end, 1, true), execute_interview(start, 1), plan_interview(end, 1), plan_interview(start, 1), check_application_for_interview(end, 1, true), check_application_for_interview(start, 1), assign_contact_partner(end, 1), verify_prerequisites(end, 1), verify_prerequisites(start, 1), assign_contact_partner(start, 1), organize_documents(end, 1), organize_documents(start, 1), check_validity(end, 1, false), check_validity(start, 1), acquire(1, company), application_sent(1), prepare_application(end, 1), prepare_application(start, 1), acquire(1, applicant), job_needed(1)], proc(sim(bpmn_process), E), trans_star(E, [], _, H2))).
+% 6,826,598 inferences, 0.315 CPU in 0.315 seconds (100% CPU, 21667041 Lips)
+false.
+
+?- time((H2 = [execute_interview(end, 1, true), execute_interview(start, 1), plan_interview(end, 1), plan_interview(start, 1), check_application_for_interview(end, 1, true), check_application_for_interview(start, 1), assign_contact_partner(end, 1), verify_prerequisites(end, 1), verify_prerequisites(start, 1), assign_contact_partner(start, 1), organize_documents(end, 1), organize_documents(start, 1), check_validity(end, 1, false), check_validity(start, 1), acquire(1, company), application_sent(1), prepare_application(end, 1), prepare_application(start, 1), acquire(1, applicant), job_needed(1)], proc(sim(bpmn_process), E), append(H1, H3, H2), trans_star(E, [], _, H3))), last(H1, A), \+ trans_star(E, [], _, [A|H3]).
+% 81,662,937 inferences, 3.648 CPU in 3.648 seconds (100% CPU, 22385459 Lips)
+H2 = [execute_interview(end, 1, true), execute_interview(start, 1), plan_interview(end, 1), plan_interview(start, 1), check_application_for_interview(end, 1, true), check_application_for_interview(start, 1), assign_contact_partner(end, 1), verify_prerequisites(end, 1), verify_prerequisites(..., ...)|...],
+E = conc([bpmn_process, end_bpm], exog_actions),
+H1 = [execute_interview(end, 1, true), execute_interview(start, 1), plan_interview(end, 1), plan_interview(start, 1), check_application_for_interview(end, 1, true), check_application_for_interview(start, 1), assign_contact_partner(end, 1), verify_prerequisites(end, 1), verify_prerequisites(..., ...)|...],
+H3 = [check_validity(end, 1, false), check_validity(start, 1), acquire(1, company), application_sent(1), prepare_application(end, 1), prepare_application(start, 1), acquire(1, applicant), job_needed(1)],
+A = organize_documents(start, 1)
+
+% VERIFICATION OF PROPERTY
+?- time(do(reasoning_task, [], H)).
+% 21,914,474 inferences, 0.812 CPU in 0.813 seconds (100% CPU, 26971717 Lips)
+H = [end_bpm, withdrawal_handled(1), process_withdrawal(end, 1), process_withdrawal(start, 1), withdrawal_completed(1), withdrawal_sent(1), confirm_withdrawal(end, 1), confirm_withdrawal(start, 1), withdrawal_by_applicant(...)|...] .
+```
+
---
+++ static_formalization/job_bpmn/job.pl
@@ -0,0 +1,455 @@
+/*
+  Complete job application BPMN with exogenous actions to control the process flow.
+*/
+
+:- dynamic controller/1.
+:- discontiguous
+    fun_fluent/1,
+    rel_fluent/1,
+    proc/2,
+    causes_true/3,
+    causes_false/3.
+
+% There is nothing to do caching on (required becase cache/1 is static)
+cache(_) :- fail.
+
+
+id(X) :- ground(X), !.
+id(X) :- between(1, 1, X).
+
+% Domain-dependent Relational Fluents
+rel_fluent(active(_ID)).
+causes_true(job_needed(ID), active(ID), true).
+causes_false(withdrawal_handled(ID), active(ID), true).
+causes_false(application_finalised(ID), active(ID), true).
+causes_false(withdrawal_completed(ID), active(ID), true).
+
+rel_fluent(processed(_ID)).
+causes_true(application_analysed(ID), processed(ID), true).
+causes_true(withdrawal_handled(ID), processed(ID), true).
+
+rel_fluent(documents_ok(_ID)).
+causes_true(check_validity(end,  ID, RESULT), documents_ok(ID), RESULT = true).
+causes_false(check_validity(end,  ID, RESULT), documents_ok(ID), RESULT = false).
+
+rel_fluent(interview(_ID)).
+causes_true(check_application_for_interview(end,  ID, RESULT), interview(ID), RESULT = true).
+causes_false(check_application_for_interview(end,  ID, RESULT), interview(ID), RESULT = false).
+
+rel_fluent(job_offer(_ID)).
+causes_true(execute_interview(end,  ID, RESULT), job_offer(ID), RESULT = true).
+causes_false(execute_interview(end,  ID, RESULT), job_offer(ID), RESULT = false).
+
+rel_fluent(approval(_ID)).
+causes_true(validate_job_offer(end,  ID, RESULT), approval(ID), RESULT = true).
+causes_false(validate_job_offer(end,  ID, RESULT), approval(ID), RESULT = false).
+
+
+rel_fluent(application(_ID)).
+causes_true(prepare_application(end,  ID), application(ID), true).
+
+rel_fluent(contract(_ID)).
+causes_true(produce_contract(end,  ID), contract(ID), true).
+
+rel_fluent(signed_contract(_ID)).
+causes_true(sign_contract(end,  ID), signed_contract(ID), true).
+
+rel_fluent(letter_of_refusal(_ID)).
+causes_true(produce_letter_of_refusal(end,  ID), letter_of_refusal(ID), true).
+
+rel_fluent(pool(P)) :- member(P, [applicant, company]).
+
+rel_fluent(waiting(_ID, _POOL)).
+causes_true(job_needed(ID), waiting(ID, applicant), true).
+causes_true(application_sent(ID), waiting(ID, company), true).
+causes_false(application_finalised(ID), waiting(ID, applicant), true).
+causes_false(application_analysed(ID), waiting(ID, company), true).
+causes_false(acquire(ID, POOL), waiting(ID, POOL), true).
+
+rel_fluent(servers_stopped).
+causes_true(shut_down, servers_stopped, true).
+
+
+% Actions and Preconditions
+%
+prim_action(prepare_application(start, _ID)).
+poss(prepare_application(start, ID), done(job_needed(ID))).
+
+prim_action(application_sent(_ID)).
+poss(application_sent(ID), and(done(prepare_application(end,  ID)), application(ID))).
+
+prim_action(check_validity(start, _ID)).
+poss(check_validity(start, ID), and(done(application_sent(ID)), application(ID))).
+
+prim_action(organize_documents(start, _ID)).
+poss(organize_documents(start, ID), documents_ok(ID)).
+
+prim_action(verify_prerequisites(start, _ID)).
+poss(verify_prerequisites(start, ID), done(organize_documents(end,  ID))).
+
+prim_action(assign_contact_partner(start, _ID)).
+poss(assign_contact_partner(start, ID), done(organize_documents(end,  ID))).
+
+prim_action(check_application_for_interview(start, _ID)).
+poss(check_application_for_interview(start, ID), and(and(done(assign_contact_partner(end,  ID)), done(verify_prerequisites(end,  ID))), application(ID))).
+
+prim_action(plan_interview(start, _ID)).
+poss(plan_interview(start, ID), interview(ID)).
+
+prim_action(execute_interview(start, _ID)).
+poss(execute_interview(start, ID), done(plan_interview(end,  ID))).
+
+prim_action(obtain_approval(start, _ID)).
+poss(obtain_approval(start, ID), job_offer(ID)).
+
+prim_action(validate_job_offer(start, _ID)).
+poss(validate_job_offer(start, ID), done(obtain_approval(end,  ID))).
+
+prim_action(produce_contract(start, _ID)).
+poss(produce_contract(start, ID), approval(ID)).
+
+prim_action(contract_sent(_ID)).
+poss(contract_sent(ID), and(done(produce_contract(end,  ID)), contract(ID))).
+
+prim_action(sign_contract(start, _ID)).
+poss(sign_contract(start, ID), and(done(contract_sent(ID)), contract(ID))).
+
+prim_action(signed_contract_sent(_ID)).
+poss(signed_contract_sent(ID), and(done(sign_contract(end,  ID)), signed_contract(ID))).
+
+prim_action(store_signed_contract(start, _ID)).
+poss(store_signed_contract(start, ID), and(done(signed_contract_sent(ID)), signed_contract(ID))).
+
+prim_action(communicate_recruitment(start, _ID)).
+poss(communicate_recruitment(start, ID), and(done(signed_contract_sent(ID)), signed_contract(ID))).
+
+prim_action(application_analysed(_ID)).
+poss(application_analysed(ID), and(done(application_finalised(ID)), and(neg(done(withdrawal_by_applicant(ID))), or(done(store_signed_contract(end,  ID)), done(letter_of_refusal_sent(ID)))))).
+
+prim_action(produce_letter_of_refusal(start, _ID)).
+poss(produce_letter_of_refusal(start, ID), neg(and(documents_ok(ID), and(interview(ID), and(job_offer(ID), approval(ID)))))).
+
+prim_action(letter_of_refusal_sent(_ID)).
+poss(letter_of_refusal_sent(ID), and(done(produce_letter_of_refusal(end,  ID)), letter_of_refusal(ID))).
+
+prim_action(application_finalised(_ID)).
+poss(application_finalised(ID), or(done(letter_of_refusal_sent(ID)), done(communicate_recruitment(end,  ID)))).
+
+prim_action(confirm_withdrawal(start, _ID)).
+poss(confirm_withdrawal(start, ID), done(withdrawal_by_applicant(ID))).
+
+prim_action(withdrawal_completed(_ID)).
+poss(withdrawal_completed(ID), done(withdrawal_sent(ID))).
+
+prim_action(withdrawal_sent(_ID)).
+poss(withdrawal_sent(ID), done(confirm_withdrawal(end,  ID))).
+
+prim_action(process_withdrawal(start, _ID)).
+poss(process_withdrawal(start, ID), done(withdrawal_sent(ID))).
+
+prim_action(withdrawal_handled(_ID)).
+poss(withdrawal_handled(ID), and(done(withdrawal_completed(ID)), done(process_withdrawal(end,  ID)))).
+
+prim_action(acquire(_ID, _POOL)).
+poss(acquire(ID, POOL), and(id(ID), and(waiting(ID, POOL), pool(POOL)))).
+
+% Exogenous Action
+%
+prim_action(Act) :- exog_action(Act).
+
+exog_action(job_needed(ID)) :- id(ID).
+poss(job_needed(ID), neg(done(job_needed(ID)))).
+
+exog_action(prepare_application(end,  ID)) :- id(ID).
+poss(prepare_application(end,  ID), running(prepare_application(start, ID))).
+
+exog_action(check_validity(end,  ID, RESULT)) :- id(ID), member(RESULT, [true, false]).
+poss(check_validity(end,  ID, _RESULT), and(running(check_validity(start, ID)), application(ID))).
+
+exog_action(organize_documents(end,  ID)) :- id(ID).
+poss(organize_documents(end,  ID), running(organize_documents(start, ID))).
+
+exog_action(verify_prerequisites(end,  ID)) :- id(ID).
+poss(verify_prerequisites(end,  ID), running(verify_prerequisites(start, ID))).
+
+exog_action(assign_contact_partner(end,  ID)) :- id(ID).
+poss(assign_contact_partner(end,  ID), running(assign_contact_partner(start, ID))).
+
+exog_action(check_application_for_interview(end,  ID, RESULT)) :-  id(ID), member(RESULT, [true, false]).
+poss(check_application_for_interview(end,  ID, _RESULT), and(running(check_application_for_interview(start, ID)), application(ID))).
+
+exog_action(plan_interview(end,  ID)) :- id(ID).
+poss(plan_interview(end,  ID), running(plan_interview(start, ID))).
+
+exog_action(execute_interview(end,  ID, RESULT)) :- id(ID), member(RESULT, [true, false]).
+poss(execute_interview(end,  ID, _RESULT), running(execute_interview(start, ID))).
+
+exog_action(obtain_approval(end,  ID)) :- id(ID).
+poss(obtain_approval(end,  ID), running(obtain_approval(start, ID))).
+
+exog_action(produce_contract(end,  ID)) :- id(ID).
+poss(produce_contract(end,  ID), running(produce_contract(start, ID))).
+
+exog_action(validate_job_offer(end,  ID, RESULT)) :-  id(ID), member(RESULT, [true, false]).
+poss(validate_job_offer(end,  ID, _RESULT), running(validate_job_offer(start, ID))).
+
+exog_action(sign_contract(end,  ID)) :- id(ID).
+poss(sign_contract(end,  ID), and(running(sign_contract(start, ID)), contract(ID))).
+
+exog_action(communicate_recruitment(end,  ID)) :- id(ID).
+poss(communicate_recruitment(end,  ID), running(communicate_recruitment(start, ID))).
+
+exog_action(store_signed_contract(end,  ID)) :- id(ID).
+poss(store_signed_contract(end,  ID), and(running(store_signed_contract(start, ID)), signed_contract(ID))).
+
+exog_action(produce_letter_of_refusal(end,  ID)) :- id(ID).
+poss(produce_letter_of_refusal(end,  ID), running(produce_letter_of_refusal(start, ID))).
+
+exog_action(withdrawal_by_applicant(ID)) :- id(ID).
+poss(withdrawal_by_applicant(ID), and(pool(POOL), and(neg(done(application_analysed(ID))), and(neg(done(application_finalised(ID))), and(neg(waiting(ID, POOL)), and(neg(processed(ID)), and(active(ID), neg(done(withdrawal_by_applicant(ID)))))))))).
+
+exog_action(confirm_withdrawal(end,  ID)) :- id(ID).
+poss(confirm_withdrawal(end,  ID), running(confirm_withdrawal(start, ID))).
+
+exog_action(process_withdrawal(end,  ID)) :- id(ID).
+poss(process_withdrawal(end,  ID), running(process_withdrawal(start, ID))).
+
+exog_action(shut_down).
+poss(shut_down, and(neg(servers_stopped), neg(active_instances_check))).
+
+
+
+% ABBREVIATIONS
+proc(running(A1), and(done(A1), neg(done(A2)))) :-
+  member(A1,
+    [check_validity(start, _),
+      check_application_for_interview(start, _),
+      execute_interview(start, _),
+      validate_job_offer(start, _)]), !,
+  A1 =.. [F|[start|L]], append(L, [_], L2), A2 =.. [F|[end|L2]].
+
+% proc(running(check_validity(start, N)),
+%   and(
+%     done(check_validity(start, N)),
+%     neg(done(check_validity(end, N, _))))) :- !.
+
+
+% default running/1 when start and end actions have the same number of arguments
+proc(running(A1), and(done(A1), neg(done(A2)))) :-
+  A1 =.. [F|[start|L]], A2 =.. [F|[end|L]].
+proc(active_instances_check, some(id, active(id))).
+
+% Initial Situation
+%
+initially(active(_ID), false).
+initially(documents_ok(_ID), false).
+initially(interview(_ID), false).
+initially(job_offer(_ID), false).
+initially(approval(_ID), false).
+initially(processed(_ID), false).
+initially(application(_ID), false).
+initially(contract(_ID), false).
+initially(signed_contract(_ID), false).
+initially(letter_of_refusal(_ID), false).
+initially(pool(_P), true).
+initially(waiting(_ID, _POOL), false).
+initially(servers_stopped, false).
+
+
+/* TOP-LEVEL APPLICATION CONTROLLER
+
+The top level BPM process involves two process in priority:
+
+1. At the top level, run concurrently the two servers for the applicant and the company.
+2. If the servers are "stuck", just wait for exogenous actions to unblock them.
+*/
+proc(control(bpmn_process), [prioritized_interrupts(
+    [
+      bpmn_process,
+      interrupt(neg(servers_stopped), ?(wait_exog_action))
+    ])]).
+
+proc(bpmn_process, conc(server_applicant, server_company)).
+
+
+/* SERVER FOR APPLICANT
+
+This implements a server for the applicant side process.
+
+It uses exogenous actions for the start event, for the withdrawal, and to shut down the servers/pools.
+*/
+proc(server_applicant, iconc(pi(app, [acquire(app, applicant), handle_applicant(app)]))).
+
+proc(handle_applicant(ID),
+  [ gexec(and(active(ID), neg(done(withdrawal_by_applicant(ID)))), applicant(ID)),
+    if(and(neg(processed(ID)), done(withdrawal_by_applicant(ID))),
+          [
+               confirm_withdrawal(start, ID),
+               ?(done(confirm_withdrawal(end,  ID))),
+               withdrawal_sent(ID),
+               withdrawal_completed(ID)
+          ],
+          []  % empty else
+      )
+  ]
+).
+
+proc(applicant(ID),
+  [prepare_application(start, ID),
+  ?(done(prepare_application(end,  ID))),
+
+    application_sent(ID),
+
+      ndet(
+        [?(done(contract_sent(ID))),
+          sign_contract(start, ID),
+          ?(done(sign_contract(end,  ID))),
+          [signed_contract_sent(ID),
+                  communicate_recruitment(start, ID),
+                  ?(done(communicate_recruitment(end,  ID))),
+                  application_finalised(ID)]
+        ],
+        [?(done(letter_of_refusal_sent(ID))), application_finalised(ID)])
+  ]
+).
+
+
+/* SERVER FOR COMPANY
+
+This implements a server for the company side process.
+
+It uses exogenous actions for the start event, for the withdrawal, and to shut down the servers/pools.
+*/
+proc(server_company, iconc(pi(app, [acquire(app, company), handle_company(app)]))).
+
+proc(handle_company(ID),
+  [ gexec(and(neg(processed(ID)), neg(done(withdrawal_sent(ID)))), company(ID)),
+    if(and(neg(processed(ID)), done(withdrawal_sent(ID))),
+          [
+            process_withdrawal(start, ID),
+            ?(done(process_withdrawal(end,  ID))),
+            withdrawal_handled(ID)
+          ],
+          []  % empty else
+      )
+  ]
+).
+
+proc(company(ID),
+
+      [
+        ?(done(application_sent(ID))),
+        check_validity(start, ID),
+        ?(some(result, done(check_validity(end,  ID, result)))),
+        if(documents_ok(ID),
+          [
+            organize_documents(start, ID),
+            ?(done(organize_documents(end,  ID))),
+            conc(assign_contact_partner(start, ID), verify_prerequisites(start, ID)),
+            ?(and(done(verify_prerequisites(end,  ID)), done(assign_contact_partner(end,  ID)))),
+            check_application_for_interview(start, ID),
+            ?(some(res, done(check_application_for_interview(end,  ID, res)))),
+            if(interview(ID),
+              [
+                plan_interview(start, ID),
+                ?(done(plan_interview(end,  ID))),
+                execute_interview(start, ID),
+                ?(some(res, done(execute_interview(end,  ID, res)))),
+                if(job_offer(ID),
+                  [
+                    obtain_approval(start, ID),
+                    ?(done(obtain_approval(end,  ID))),
+                    validate_job_offer(start, ID),
+                    ?(some(res, done(validate_job_offer(end,  ID, res)))),
+                    if(approval(ID),
+                      [
+                        produce_contract(start, ID),
+                        ?(done(produce_contract(end,  ID))),
+                          [
+                            contract_sent(ID),
+                              [
+                                ?(done(signed_contract_sent(ID))),
+                                store_signed_contract(start, ID),
+                                ?(done(store_signed_contract(end,  ID))),
+                                application_analysed(ID)
+                              ]
+                          ]
+                      ],
+                      [
+                        produce_letter_of_refusal(start, ID),
+                        ?(done(produce_letter_of_refusal(end,  ID))),
+                        [
+                            letter_of_refusal_sent(ID),
+                            application_analysed(ID)
+                        ]
+                      ]
+                    )
+                  ],
+                  [
+                    produce_letter_of_refusal(start, ID),
+                    ?(done(produce_letter_of_refusal(end,  ID))),
+                    [
+                        letter_of_refusal_sent(ID),
+                        application_analysed(ID)
+                    ]
+                  ]
+                )
+              ],
+              [
+                produce_letter_of_refusal(start, ID),
+                ?(done(produce_letter_of_refusal(end,  ID))),
+                [
+                    letter_of_refusal_sent(ID),
+                    application_analysed(ID)
+                ]
+
+              ]
+            )
+          ],
+          [
+            produce_letter_of_refusal(start, ID),
+            ?(done(produce_letter_of_refusal(end,  ID))),
+            [
+                letter_of_refusal_sent(ID),
+                application_analysed(ID)
+            ]
+          ]
+        )
+      ]
+).
+
+% proc(exog_actions, star(exog_action)).
+
+prim_action(end_bpm).
+poss(end_bpm, true).
+
+proc(exog_actions,
+  if(done(end_bpm), [], [exog_action, exog_actions])).
+
+
+proc(exog_action, pi(a, [?(and(exog_action(a), neg(system_action(a)))), a])).
+proc(control(reasoning_task), search(reasoning_task)).
+
+% simulate process BP under exogenous events
+proc(sim(BP), conc([BP, end_bpm], exog_actions)).
+
+proc(reasoning_task,
+  [
+    conc([bpmn_process, end_bpm], exog_actions),
+    ?(some([id],
+          and(signed_contract(id),
+              neg(done(application_finalised(id))))))
+  ] % end of search
+).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  INFORMATION FOR THE EXECUTOR
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Translations of domain actions to real actions (one-to-one)
+actionNum(X, X).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% EOF
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
